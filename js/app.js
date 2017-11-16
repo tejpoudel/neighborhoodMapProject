e1fcfa9b4e4bbb70f8e52e09ca82206d479e2bb6
Tej Poudel <tejpoudel@Tejs-MacBook-Pro.local>
2017-11-16 03:08:10 -0800

    fix: fix some unclear functions and html elements.

--- js/app.js
+++ js/app.js
@@ -88,7 +88,7 @@ function mapModel() {
         var stops = [
             { title: 'Greyhound:Bus Stop', lat: 38.4382333, lng:  -122.71320479999997, type: 'Public Transportation'},
             { title: 'Greyhound Lines', lat: 38.395705, lng: -122.71447439999997, type: 'Public Transportation'},
-            { title: 'Bus Schedule-Route Information', lat: 38.4379143, lng: -122.71146959999999, type:  'Public Transportation Information'},
+            { title: 'Bus Schedule Route Information', lat: 38.4379143, lng: -122.71146959999999, type:  'Public Transportation Information'},
             { title: 'Sonoma County Transit', lat: 38.3940118, lng: -122.72268889999998, type:  'Public Transportation'},
             { title: 'Sonoma County Airport Train Stop', lat: 38.510787, lng: -122.78450700000002, type: 'Public Transportation'},
             { title: 'Santa Rosa North Train Stop', lat: 38.4551412, lng: -122.7363881, type: 'Public Transportation'},
@@ -205,8 +205,5 @@ var populateAndBounceMarker = function() {
 
 function startApp() {
     ko.applyBindings(new mapModel());
-
-
-
 
 }
